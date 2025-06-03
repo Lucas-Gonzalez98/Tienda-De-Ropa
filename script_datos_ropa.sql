ca52ccde595a1fcf2b8845df028cf060a54b70ec
JuanCruzGonzalez <juancgonzalez0901@gmail.com>
2025-06-02 22:14:53 -0300

    actualizacion de script de usuario

--- script_datos_ropa.sql
+++ script_datos_ropa.sql
@@ -1,29 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 03-06-2025 a las 03:08:18
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `tienda_ropa`
---
-
---
--- Volcado de datos para la tabla `administrador`
---
+USE tienda_ropa;
 
 INSERT INTO `administrador` (`id`, `eliminado`, `apellido`, `nombre`, `id_domicilio`, `id_telefono`, `id_usuario`) VALUES
 (1, b'0', 'Rodriguez', 'Manuel', 1, 1, 1);
