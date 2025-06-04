a7e4d04a5505c76227d20c52ea37841808ba2e49
LucasGonzalez <topolucasnicolas@gmail.com>
2025-06-04 19:45:06 -0300

    arreglando modelos del front

--- FrontEnd/src/models/ArticuloInsumo.ts
+++
@@ -1,8 +0,0 @@
-import Articulo from "./Articulo";
-import SucursalInsumo from "./SucursalInsumo";
-
-export default class ArticuloInsumo extends Articulo {
-    precioCompra: number = 0;
-    esParaElaborar!: boolean;
-    sucursalInsumo!: SucursalInsumo;
-}
