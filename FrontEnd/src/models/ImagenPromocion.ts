0ab50d24a602dcc9689436b09ed968b72ba92b2d
LucasGonzalez <topolucasnicolas@gmail.com>
2025-06-05 10:29:52 -0300

    Arreglando el modelo de entidades del FrontEnd

---
+++ FrontEnd/src/models/ImagenPromocion.ts
@@ -0,0 +1,7 @@
+import Promocion from "./Promocion";
+export default class ImagenPromocion {
+    id?: number;
+    denominacion: string = "";
+    promocion?: Promocion;
+    eliminado!: boolean;
+}
