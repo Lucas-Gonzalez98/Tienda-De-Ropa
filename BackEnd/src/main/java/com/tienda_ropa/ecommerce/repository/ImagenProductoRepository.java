a9357e93eb2b4ed1860650d12f17357eefb68dd8
LucasGonzalez <topolucasnicolas@gmail.com>
2025-06-05 10:08:52 -0300

    Gestion de Stock: Poder agregarle mas stock a un producto, con su talle y color. Gestion de Imagenes: Agregar imagenes en base64 y Buscar imagenes por producto

--- BackEnd/src/main/java/com/tienda_ropa/ecommerce/repository/ImagenProductoRepository.java
+++ BackEnd/src/main/java/com/tienda_ropa/ecommerce/repository/ImagenProductoRepository.java
@@ -2,5 +2,11 @@ package com.tienda_ropa.ecommerce.repository;
 
 import com.tienda_ropa.ecommerce.model.ImagenProducto;
 
+import java.util.List;
+
 public interface ImagenProductoRepository extends MasterRepository<ImagenProducto, Long> {
+
+    //obtener imágenes por producto
+    List<ImagenProducto> findByProductoId(Long idProducto);
+
 }
