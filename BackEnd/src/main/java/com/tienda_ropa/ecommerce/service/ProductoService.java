9b5253f9853eb3befd01eabedbc6c914a7b17d6a
LucasGonzalez <topolucasnicolas@gmail.com>
2025-06-04 23:30:25 -0300

    Arregle la creacion de un producto, desde el back

--- BackEnd/src/main/java/com/tienda_ropa/ecommerce/service/ProductoService.java
+++ BackEnd/src/main/java/com/tienda_ropa/ecommerce/service/ProductoService.java
@@ -3,10 +3,14 @@ package com.tienda_ropa.ecommerce.service;
 import com.tienda_ropa.ecommerce.model.Producto;
 import com.tienda_ropa.ecommerce.model.Stock;
 
+import java.util.List;
 import java.util.Set;
 
 public interface ProductoService extends MasterService<Producto, Long> {
 
-    Producto crearProductoCompleto(Producto producto, Set<Stock> stockInicial,
-                                   Double precioVentaInicial, Double precioCompraInicialOpcional);
+
+    //Guardar un producto (CREAR)
+    Producto crearProductoCompleto(Producto producto, Set<Stock> stock,
+                                   Double precioVentaInicial, Double precioCompraInicialOpcional,
+                                   List<String> imagenesBase64);
 }
