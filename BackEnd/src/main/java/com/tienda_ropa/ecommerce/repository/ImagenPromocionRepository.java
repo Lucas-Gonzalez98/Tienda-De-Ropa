287a678e106a2e4551bd0d042c28d80038bf50f1
LucasGonzalez <topolucasnicolas@gmail.com>
2025-06-09 13:24:30 -0300

    Eliminacion de todo lo referido a promocion, tanto en el back como en el front

--- BackEnd/src/main/java/com/tienda_ropa/ecommerce/repository/ImagenPromocionRepository.java
+++
@@ -1,6 +0,0 @@
-package com.tienda_ropa.ecommerce.repository;
-
-import com.tienda_ropa.ecommerce.model.ImagenPromocion;
-
-public interface ImagenPromocionRepository extends MasterRepository<ImagenPromocion, Long> {
-}
--- FrontEnd/src/models/PromocionDetalle.ts
+++
@@ -1,7 +0,0 @@
-
-export default class PromocionDetalle{
-
-    cantidad: number = 0;
-    productoId!: number;
-
-}
